03b15f964ddff4520144b9990aeea9e205932f13
= <=>
2026-09-07 11:22:57 +0530

    Initial commit

--- Tests/mocks/DobbyMock.h
+++ Tests/mocks/DobbyMock.h
@@ -70,6 +70,8 @@ public:
                          const std::string& command), (const, override));
     MOCK_METHOD(int, registerListener, (const StateChangeListener &listener, const void* cbParams), (override));
     MOCK_METHOD(void, unregisterListener, (int tag), (override));
+    MOCK_METHOD(int, registerListenerWithStatus, (const StateChangeListenerWithStatus &listener, const void* cbParams), (override));
+    MOCK_METHOD(void, unregisterListenerWithStatus, (int tag), (override));
     MOCK_METHOD(std::string, getContainerInfo, (int32_t descriptor), (const, override));
     MOCK_METHOD((std::list<std::pair<int32_t, std::string>>), listContainers, (), (const));
 
