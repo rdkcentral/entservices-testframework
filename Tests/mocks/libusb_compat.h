bf4c185463895b1ba6a268dd3b4a49d606d1adcf
balav08 <54432605+balav08@users.noreply.github.com>
2026-09-17 12:17:06 +0530

    RDKEMW-23188 : Update libusb_compat.h

--- Tests/mocks/libusb_compat.h
+++ Tests/mocks/libusb_compat.h
@@ -4,11 +4,8 @@
  *
  * Copyright 2026 RDK Management
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the GNU Lesser General Public License, version 2.1 or later.
+ * See the COPYING file at the root directory of this project for the complete license.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
